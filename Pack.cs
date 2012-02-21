bab03f74f3daa8541403e971462bd3c9af51f0d6
Graham Darnell <graham.darnell@linn.co.uk>
2012-02-21 15:03:06 +0000

    Minor changes

--- Pack.cs
+++ Pack.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.IO;
+using System.IO.Compression;
 
 using ICSharpCode.SharpZipLib.Zip.Compression;
 using ICSharpCode.SharpZipLib.Zip.Compression.Streams;
